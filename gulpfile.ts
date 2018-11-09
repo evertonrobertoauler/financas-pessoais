5a5c4018c781adfe38a8e0067852655bb68178da
Everton R. Auler <evertonrobertoauler@gmail.com>
2018-11-09 13:32:53 -0200

    adicionando suporte ao android sdk >= 16 <23

--- gulpfile.ts
+++ gulpfile.ts
@@ -19,6 +19,62 @@ const addCordovaScript = () =>
 
 const copyConfig = cb => shellTask(['cp -rf config.xml cordova/config.xml'])(cb);
 
+const copyConfig16 = cb => shellTask(['cp -rf config-16.xml cordova/config.xml'])(cb);
+
+const fixBaseUrl16 = () =>
+  src(['cordova/www/index.html'])
+    .pipe(replace('<base href="/">', `<base href="file:///android_asset/www/" />`))
+    .pipe(dest('cordova/www/'));
+
+const jsFetch = `
+  var originalFetch = window.fetch;
+
+  window.fetch = function () {
+      var args = [];
+      for (var _i = 0; _i < arguments.length; _i++) {
+          args[_i] = arguments[_i];
+      }
+      var url = args[0];
+      if (typeof url === 'string' && url.match(/\.svg/)) {
+          return new Promise(function (resolve, reject) {
+              var req = new XMLHttpRequest();
+              req.open('GET', url, true);
+              req.addEventListener('load', function () {
+                  resolve({ ok: true, text: function () { return Promise.resolve(req.responseText); } });
+              });
+              req.addEventListener('error', reject);
+              req.send();
+          });
+      }
+      else {
+          return originalFetch.apply(void 0, args);
+      }
+  };
+`;
+
+const addFetch16 = () =>
+  src(['cordova/www/index.html'])
+    .pipe(replace('</head>', `<script type="text/javascript">${jsFetch}</script></head>`))
+    .pipe(dest('cordova/www/'));
+
+const fixAndroidGradleFlavor = () =>
+  src(['cordova/platforms/android/build.gradle'])
+    .pipe(replace('android {', `android {\n  flavorDimensions 'default'`))
+    .pipe(dest('cordova/platforms/android/'));
+
+const scriptSupportVersion = `
+  configurations.all {
+    resolutionStrategy {
+        force 'com.android.support:support-v4:27.1.0'
+    }
+  }
+`;
+
+const fixAndroidGradleRepository = () =>
+  src(['cordova/platforms/android/build.gradle'])
+    .pipe(replace('allprojects', `${scriptSupportVersion}\nallprojects`))
+    .pipe(dest('cordova/platforms/android/'));
+
 const replaceWebClientId = replace(
   /"WEB_APPLICATION_CLIENT_ID"\s+value=""/gi,
   `"WEB_APPLICATION_CLIENT_ID" value="${webClientId}"`
@@ -37,6 +93,13 @@ const cordovaAddAndroid = cb => shellTask(['cd cordova; cordova platform add and
 
 const cordovaRunAndroid = cb => shellTask(['cd cordova; cordova run android'])(cb);
 
+const cordovaBuildAndroid = cb => shellTask(['cd cordova; cordova build android'])(cb);
+
+const apk = 'cordova/platforms/android/build/outputs/apk/armv7/debug/android-armv7-debug.apk';
+const cordovaInstallAndroid16 = cb => shellTask([`adb install -r ${apk}`])(cb);
+
+const cordovaRunAndroid16 = series(cordovaBuildAndroid, cordovaInstallAndroid16);
+
 export const clean = cb => shellTask(['rm -rf cordova'])(cb);
 
 export const addAndroid = series(
@@ -45,6 +108,16 @@ export const addAndroid = series(
   copyGoogleConfig,
   copyResourses,
   cordovaAddAndroid
+);
+
+export const addAndroid16 = series(
+  copyConfig16,
+  setWebClientId,
+  copyGoogleConfig,
+  copyResourses,
+  cordovaAddAndroid,
+  fixAndroidGradleFlavor,
+  fixAndroidGradleRepository
 );
 
 export const runAndroidRemote =
@@ -61,3 +134,10 @@ export const runAndroidProd =
   existsSync('cordova/www') && existsSync('cordova/platforms/android')
     ? series(buildProd, addCordovaScript, cordovaRunAndroid)
     : series(clean, buildProd, addCordovaScript, addAndroid, cordovaRunAndroid);
+
+const fixIndexHtml16 = series(addCordovaScript, fixBaseUrl16, addFetch16);
+
+export const runAndroid16 =
+  existsSync('cordova/www') && existsSync('cordova/platforms/android')
+    ? series(buildDev, fixIndexHtml16, cordovaRunAndroid16)
+    : series(clean, buildDev, fixIndexHtml16, addAndroid16, cordovaRunAndroid16);
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -81,7 +81,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   providers: [...GUARDAS],
   exports: [RouterModule]
 })
