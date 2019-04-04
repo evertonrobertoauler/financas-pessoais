6052c6644dffcbc2232d6f8115b39f358392d00c
Éverton Roberto Auler <everton@localhost.localdomain>
2019-04-03 21:48:19 -0300

    adicionando mais alguns testes

--- e2e/cypress/integration/ct_01.spec.ts
+++ e2e/cypress/integration/ct_01.spec.ts
@@ -1,7 +1,6 @@
 context('CT_01', () => {
   before(() => {
-    cy.exec('gulp limparFirestore')
-      .visit('http://localhost:4200');
+    cy.exec('gulp limparFirestore').visit('http://localhost:4200');
   });
 
   it('1. Inserir um caixa financeiro', () => {
@@ -28,7 +27,7 @@ context('CT_01', () => {
     cy.wait(200);
 
     cy.get('app-inicio .item-caixa').should('have.length', 1);
-    cy.get('app-inicio .item-caixa h2 b').should('have.text', 'Banco Inter');
+    cy.get('app-inicio .item-caixa:eq(0) h2 b').should('have.text', 'Banco Inter');
   });
 
   it('2. Inserir um Caixa Financeiro de um Cartão de Crédito', () => {
@@ -58,5 +57,46 @@ context('CT_01', () => {
 
     cy.get('app-inicio .item-caixa').should('have.length', 2);
     cy.get('app-inicio .item-caixa:eq(1) h2 b').should('have.text', 'Mastercard Santander');
+  });
+
+  it('3. Editar um Caixa Financeiro', () => {
+    cy.get('app-inicio ion-header ion-title').should('have.text', 'Saldo');
+    cy.get('app-inicio ion-content .item-caixa:eq(0) ion-button')
+      .first()
+      .click();
+
+    cy.wait(1000);
+    cy.get('app-caixa-financeiro ion-header ion-title').should('contain', 'Caixa Financeiro');
+    cy.get('app-caixa-financeiro [formControlName="nome"] input').clear().type('Mastercard Bradesco');
+
+    cy.get('app-caixa-financeiro ion-button[type="submit"]')
+      .should('contain', 'Salvar')
+      .click();
+
+    cy.wait(200);
+
+    cy.get('app-inicio .item-caixa').should('have.length', 2);
+    cy.get('app-inicio .item-caixa:eq(0) h2 b').should('have.text', 'Mastercard Bradesco');
+  });
+
+  it('4. Excluir Caixa Financeiro', () => {
+    cy.get('app-inicio ion-header ion-title').should('have.text', 'Saldo');
+    cy.get('app-inicio ion-content .item-caixa:eq(1) ion-button')
+      .first()
+      .click();
+
+    cy.wait(1000);
+    cy.get('app-caixa-financeiro ion-header ion-title').should('contain', 'Caixa Financeiro');
+    cy.get('app-caixa-financeiro ion-buttons[slot="end"] ion-button').click();
+
+    cy.wait(200);
+
+    cy.get('ion-alert button').last()
+      .should('have.text', 'Sim')
+      .click();
+
+    cy.wait(200);
+
+    cy.get('app-inicio .item-caixa').should('have.length', 1);
   });
 });
