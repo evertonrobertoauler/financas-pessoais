57b0d63f6179205ef7a8a507d68ff59140955d8b
Everton R. Auler <evertonrobertoauler@gmail.com>
2019-04-04 20:41:58 -0300

    configurando jest

---
+++ src/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  verbose: true,
+  bail: true,
+  cache: false,
+  // collectCoverage: true,
+  // collectCoverageFrom: [`**/app/**/*.ts`, '!**/node_modules/**', '!**/index.ts', '!**/*.module.ts'],
+  moduleNameMapper: {}
+};
