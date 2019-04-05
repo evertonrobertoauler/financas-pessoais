48bf6081fb85ec0e36d848cdc8d285b58610f9ae
Everton R. Auler <evertonrobertoauler@gmail.com>
2019-04-04 21:10:05 -0300

    adicionados testes unitarios

--- src/app/servicos/formatar-dados.service.spec.ts
+++ src/app/servicos/formatar-dados.service.spec.ts
@@ -2,11 +2,40 @@ import { TestBed } from '@angular/core/testing';
 
 import { FormatarDadosService } from './formatar-dados.service';
 
-describe('FormatarDadosService', () => {
+describe('Testando serviço responsavel pela formatação dos dados', () => {
+  let service: FormatarDadosService;
+
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: FormatarDadosService = TestBed.get(FormatarDadosService);
-    expect(service).toBeTruthy();
+  beforeEach(() => (service = TestBed.get(FormatarDadosService)));
+
+  it('Formatação valores no formato brasileiro', () => {
+    expect(service.formatarValor(10)).toBe('10,00');
+    expect(service.formatarValor(54610.01)).toBe('54.610,01');
+    expect(service.formatarValor(-99.88)).toBe('-99,88');
+    expect(service.formatarValor(-99.889999)).toBe('-99,89');
+    expect(service.formatarValor('10')).toBe('0,10');
+    expect(service.formatarValor('-5461001')).toBe('-54.610,01');
+    expect(service.formatarValor('9988')).toBe('99,88');
+    expect(service.formatarValor('-99.8899,999')).toBe('-9.988.999,99');
+  });
+
+  it('Formatação valores do formato brasileiro devem ser convertidos de volta para valores númericos', () => {
+    expect(service.valorToNumber('10,00')).toBe(10);
+    expect(service.valorToNumber('54.610,01')).toBe(54610.01);
+    expect(service.valorToNumber('-99,88')).toBe(-99.88);
+    expect(service.valorToNumber('-99,89')).toBe(-99.89);
+    expect(service.valorToNumber('0,10')).toBe(0.1);
+    expect(service.valorToNumber('-54.610,01')).toBe(-54610.01);
+    expect(service.valorToNumber('99,88')).toBe(99.88);
+    expect(service.valorToNumber('-9.988.999,99')).toBe(-9988999.99);
+  });
+
+  it('Formatação dadas no formato brasileiro', () => {
+    expect(service.formatarData('1993-01-05', 'DD/MM/YYYY')).toBe('05/01/1993');
+    expect(service.formatarData('2001-09-31 23:59:45', 'DD/MM/YYYY HH:mm:ss')).toBe(
+      '01/10/2001 23:59:45'
+    );
+    expect(service.formatarData(new Date(2020, 11, 25), 'DD/MM/YYYY')).toBe('25/12/2020');
   });
 });
